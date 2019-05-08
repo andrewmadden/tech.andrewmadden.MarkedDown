7fd00a897b03787a572ba61c48435ca9e603cba9
andrew madden <andrew.madden@live.com.au>
2019-05-08 23:25:40 +1000

    added marklight and down to podfile

---
+++ MarkedDown/MarkedDown/Controllers/EditorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarkedDown
+//
+//  Created by andrew madden on 8/5/19.
+//  Copyright © 2019 Andrew Madden. All rights reserved.
+//
+
+import UIKit
+
+class EditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MarkedDown/MarkedDown/Controllers/PreviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PreviewViewController.swift
+//  MarkedDown
+//
+//  Created by andrew madden on 8/5/19.
+//  Copyright © 2019 Andrew Madden. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
