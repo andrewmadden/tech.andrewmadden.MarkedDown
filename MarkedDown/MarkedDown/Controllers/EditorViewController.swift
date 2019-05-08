22b694f2a8997545fea45169fe42ebe81b14b129
andrew madden <andrew.madden@live.com.au>
2019-05-08 23:56:26 +1000

    added md syntax highlighting

--- MarkedDown/MarkedDown/Controllers/EditorViewController.swift
+++ MarkedDown/MarkedDown/Controllers/EditorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  EditorViewController.swift
 //  MarkedDown
 //
 //  Created by andrew madden on 8/5/19.
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import Marklight
 
 class EditorViewController: UIViewController {
+    @IBOutlet weak var editorTextView: UITextView!
+    
+    let textStorage = MarklightTextStorage()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Add markdown syntax highlighting to text view
+        textStorage.addLayoutManager(editorTextView.layoutManager)
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
