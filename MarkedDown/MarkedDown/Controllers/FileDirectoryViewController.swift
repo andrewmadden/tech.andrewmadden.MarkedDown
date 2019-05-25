7bcce6a958c91f04beaf3cf3c75ab9ec1fb04302
andrew madden <andrew.madden@live.com.au>
2019-05-25 15:50:43 +1000

    added navigation, added new file alert

--- MarkedDown/MarkedDown/Controllers/FileDirectoryViewController.swift
+++ MarkedDown/MarkedDown/Controllers/FileDirectoryViewController.swift
@@ -10,12 +10,58 @@ import UIKit
 
 class FileDirectoryViewController: UIViewController {
 
+    let fm = FileManager.default
+    let path = Bundle.main.resourcePath!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        // add 'add file' icon in nav bar
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(newFile))
+    }
+    
+    @objc func newFile() {
+        print("new file selected")
+        // dialog to get new filename
+        let alert = UIAlertController(title: "New File", message: "Enter a file name", preferredStyle: .alert)
+        
+        // add text field for new file name
+        alert.addTextField { (textField: UITextField) in
+                textField.placeholder = "newFile"
+        }
+        
+        // add action to create button
+        alert.addAction(UIAlertAction(title: "Create", style: .default, handler: { [weak alert] (_) in
+            if let newFileName: String = alert?.textFields![0].text {
+                // create file and trigger segue to editor
+                print(newFileName)
+            } else {
+                // throw error that file could not be created
+            }
+        }))
+        
+        // add action to close dialog without doing anything to cancel button
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { [weak alert] (_) in
+            print("Create File was cancelled")
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func getFilesFromDocumentsDirectory() {
+        let files = try? fm.contentsOfDirectory(atPath: getDocumentsDirectory().absoluteString)
+        files?.forEach { print($0) }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
 
     /*
     // MARK: - Navigation
